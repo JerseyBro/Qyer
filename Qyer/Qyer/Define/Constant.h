bad31490dfc81da6daeb46f477aeef95b1331986
YangXiong <315581365@qq.com>
2016-11-23 15:06:53 +0800

    2016年11月23号下午3点06分 添加Constant头文件

---
+++ Qyer/Qyer/Define/Constant.h
@@ -0,0 +1,21 @@
+//
+//  Constant.h
+//  Qyer
+//
+//  Created by tarena15 on 2016/11/23.
+//  Copyright © 2016年 DKD. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+// 推荐页面的网络地址宏定义.
+#define ktouPath @"http://open.qyer.com/qyer/home/home_feed?client_id=qyer_ios&amp;client_secret=cd254439208ab658ddf9&count=10&lat=22.64519304434324&lon=114.1987205967858&page=1&track_app_channel=App%2520Store&track_app_version=7.0.5&amp;track_device_info=iPhone%25205s&amp;track_deviceid=FDBE0B0B-FBBE-B527-56A9-00E955C14A7C&track_os=ios%252010.1.1&amp;v=1"
+
+
+// 旅行商城页面的网络地址宏定义.
+#define kShoppingPath @"http://open.qyer.com/qyer/discount/zk/discount_index?client_id=qyer_ios&client_secret=cd254439208ab658ddf9&count=20&lat=22.64518681964929&lon=114.1987129509261&page=1&track_app_channel=App%2520Store&track_app_version=7.0.5&track_device_info=iPhone%25205&track_deviceid=30554DAE-A825-442E-95BE-82E8AEC35AD6&track_os=ios%25209.1&v=1"
+
+
+
+#endif /* Constant_h */
